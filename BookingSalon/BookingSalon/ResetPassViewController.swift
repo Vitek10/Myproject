525637c1fdc5b39cb268019aa2b11068bf5a2295
Victor <danilev12@mail.ru>
2019-02-06 18:38:33 +0200

    Update BookingSalon/BookingSalon/ResetPassViewController.swift

---
+++ BookingSalon/BookingSalon/ResetPassViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ResetPassViewController.swift
+//  BookingSalon
+//
+//  Created by Витя on 2/3/19.
+//  Copyright © 2019 Victor. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class ResetPassViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+ 
+    @IBAction func forgetPass(_ sender: UIButton) {
+        if self.emailField.text == "" {
+            let alertController = UIAlertController(title: "Oops!", message: "Please enter an email.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            Auth.auth().sendPasswordReset(withEmail: self.emailField.text!, completion: { (error) in
+                
+                var title = ""
+                var message = ""
+                
+                if error != nil {
+                    title = "Error!"
+                    message = (error?.localizedDescription)!
+                } else {
+                    title = "Success!"
+                    message = "Password reset email sent."
+                    self.emailField.text = ""
+                }
+                
+                let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+            })
+        }
+
+    }
+   
+    
+}
